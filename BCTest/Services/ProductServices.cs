4d67126915b0f789d44f2a7374cb1b262eaf1563
lqviet45 <lqviet455@gmail.com>
2023-12-13 10:56:32 +0700

    adding product services

---
+++ BCTest/Services/ProductServices.cs
@@ -0,0 +1,19 @@
+﻿namespace BCTest.Services
+{
+    public class ProductServices
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string BaseUri = "https://api.businesscentral.dynamics.com/v2.0/873a7c07-4a74-4222-a9a0-22c8560049e0";
+        private readonly TokenServices _tokenServices;
+        private readonly TokenApplicationServices _tokenApplicationServices;
+
+        public ProductServices(IHttpClientFactory httpClientFactory, TokenServices tokenServices, TokenApplicationServices tokenApplicationServices)
+        {
+            _httpClientFactory = httpClientFactory;
+            _tokenServices = tokenServices;
+            _tokenApplicationServices = tokenApplicationServices;
+        }
+
+
+    }
+}
